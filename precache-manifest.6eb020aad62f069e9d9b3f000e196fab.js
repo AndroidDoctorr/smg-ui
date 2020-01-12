c20033623538e8001d62dcd927bb3a554a267353
AndroidDoctorr <gbartthenerd@gmai.com>
2020-01-12 15:47:42 -0500

    Updates

---
+++ precache-manifest.6eb020aad62f069e9d9b3f000e196fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcd85dc6970f05114c6af3835eb7f01",
+    "url": "/smg-ui/index.html"
+  },
+  {
+    "revision": "cff028ac71354b731294",
+    "url": "/smg-ui/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "385c5a10c2724bcefe62",
+    "url": "/smg-ui/static/js/2.97a93156.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smg-ui/static/js/2.97a93156.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cff028ac71354b731294",
+    "url": "/smg-ui/static/js/main.bba57704.chunk.js"
+  },
+  {
+    "revision": "09519696c27de29edcc7",
+    "url": "/smg-ui/static/js/runtime-main.93ea715d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smg-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
