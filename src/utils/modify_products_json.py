920283104039a674eeb9316164eb4800bc2a1aa5
AndroidDoctorr <gbartthenerd@gmail.com>
2020-06-25 17:39:04 -0400

    updating product data

--- src/utils/modify_products_json.py
+++ src/utils/modify_products_json.py
@@ -1,13 +1,12 @@
 import json, csv
 
 jsonFilePath = 'products.json'
-trueLeafFilePath = 'tlproducts.csv'
+csvFilePath = 'products.csv'
 
-with open('products.csv') as csv_file:
+with open(csvFilePath) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     first_line = True
     for row in csv_reader:
         if first_line:
             print(row)
             first_line = False
-        
